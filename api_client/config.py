56c357406d932d8574477f4dac4bb9af7a0d4522
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2025-01-26 18:52:44 +0100

    ✨ Implement API client to fetch data using idealista's location IDs

--- api_client/config.py
+++ api_client/config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -9,7 +10,9 @@ API_KEY = os.getenv("IDEALISTA_API_KEY")
 CLIENT_SECRET = os.getenv("IDEALISTA_CLIENT_SECRET")
 
 if not API_KEY or not CLIENT_SECRET:
-    raise ValueError("API_KEY or CLIENT_SECRET are not set in the environment.")
+    raise ValueError(
+        "Missing required environment variables: 'IDEALISTA_API_KEY' or 'IDEALISTA_CLIENT_SECRET'"
+    )
 
 # Token URL for Idealista API
 TOKEN_URL = "https://api.idealista.com/oauth/token"
@@ -28,7 +31,6 @@ DEFAULT_SEARCH_PARAMS = {
     "order": "publicationDate",
     "sort": "desc",
     "maxPrice": "100000000",
-    "distance": "90000"
 }
 
 # Predefined centers (cities) with their coordinates
@@ -38,13 +40,23 @@ CITY_COORDINATES = {
     "barcelona": "41.385064,-2.173404",
 }
 
+# Predefined idealista location IDs for cities
+CITY_LOCATION_IDS = {
+    "lisbon": "0-EU-PT-11-06",
+    "madrid": "0-EU-ES-28-07-001-079",
+    "barcelona": "0-EU-ES-08-13-001-019",
+}
 
-def get_search_params(city, **overrides):
+
+def get_search_params(
+    city: str = "lisbon", distance_to_center: Optional[int] = None, **overrides
+) -> dict:
     """
     Returns search parameters for the specified city with optional overrides.
 
     Args:
-        city (str): The city to search (e.g., 'lisbon', 'madrid', 'barcelona').
+        city (str): The city to search (case-insensitive, e.g., 'lisbon', 'madrid', 'barcelona').
+        distance_to_center (int): Optional. The distance in meters to the city center. If not provided, the city's location ID will be used.
         **overrides: Additional search parameters to override defaults.
 
     Returns:
@@ -56,10 +68,22 @@ def get_search_params(city, **overrides):
         )
 
     search_params = DEFAULT_SEARCH_PARAMS.copy()
-    search_params.update(
-        {
-            "center": CITY_COORDINATES[city.lower()],
-        }
-    )
+
+    if distance_to_center is not None:
+        if distance_to_center <= 0:
+            raise ValueError("distance_to_center must be a positive integer.")
+        search_params.update(
+            {
+                "center": CITY_COORDINATES[city.lower()],
+                "distance": distance_to_center,
+            }
+        )
+    else:
+        search_params.update(
+            {
+                "locationId": CITY_LOCATION_IDS[city.lower()],
+            }
+        )
+
     search_params.update(overrides)  # Apply overrides if provided
     return search_params
