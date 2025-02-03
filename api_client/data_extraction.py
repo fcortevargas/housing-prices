49272dbf62dcd3c29aa03506fe9d7a5ccb23614c
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2025-02-03 22:01:59 +0100

    ♻️ Refactoring and improvements for data extraction script.

--- api_client/data_extraction.py
+++ api_client/data_extraction.py
@@ -1,7 +1,10 @@
 import datetime
 import pandas as pd
+import logging
 from api_client.api import IdealistaAPIClient
 from api_client.config import get_search_params
+
+logging.basicConfig(level=logging.INFO)
 
 
 def fetch_data_for_city(client, city, total_pages=2, **search_overrides):
@@ -20,10 +23,9 @@ def fetch_data_for_city(client, city, total_pages=2, **search_overrides):
     combined_results = []
     for page in range(1, total_pages + 1):
         params = get_search_params(city, numPage=page, **search_overrides)
+        logging.info(f"Fetching data for {city} (page {page})...")
         response = client.search(params)
-        combined_results.extend(
-            response.get("elementList", [])
-        )
+        combined_results.extend(response.get("elementList", []))
     return combined_results
 
 
@@ -43,13 +45,29 @@ def results_to_df(results):
 
 
 if __name__ == "__main__":
+    # Initialize API client
     client = IdealistaAPIClient()
 
+    # Define city, search operation, and number of pages to fetch
+    city = "lisbon"
+    operation = "rent"
+    pages = 15
+
     # Example: Fetch data for Lisbon with default params
-    lisbon_rent_results = fetch_data_for_city(client, city="lisbon", total_pages=5)
+    listings = fetch_data_for_city(
+        client=client,
+        city=city,
+        operation=operation,
+        total_pages=pages,
+    )
 
     # Convert results to DataFrames
-    rent_df = results_to_df(lisbon_rent_results)
+    df = results_to_df(listings)
 
     # Save DataFrames to CSV
-    rent_df.to_csv(f"data/raw/{datetime.date.today()}-lisbon-listings-for-rent.csv", index=False)
+    logging.info(f"Saving {len(df)} listings to CSV...")
+    df.to_csv(
+        f"data/idealista/raw/{datetime.date.today()}-{city}-listings-for-{operation}.csv",
+        index=False,
+    )
+    logging.info("Done!")
