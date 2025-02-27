2461eb61e29f5539aabc5204402a4b87da5107ea
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2025-02-27 21:14:11 +0100

    ✨ Start proper feature engineering pipeline

--- scripts/preprocessors.py
+++ scripts/preprocessors.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import KNNImputer
+from sklearn.preprocessing import MinMaxScaler
 
 from feature_engine.outliers import OutlierTrimmer
 from feature_engine.imputation import AddMissingIndicator
@@ -233,6 +234,9 @@ class FrequentValueAmputer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self, variables: list, threshold: float = 0.05):
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
         self.variables = variables
         self.threshold = threshold
         self.frequent_values = {}
@@ -285,6 +289,9 @@ class RareValueTrimmer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(self, variables: list, threshold: float = 0.05):
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
         self.variables = variables
         self.threshold = threshold
         self.rare_values = {}
@@ -337,3 +344,214 @@ class RareValueTrimmer(BaseEstimator, TransformerMixin):
         X_transformed = self.transform(X)
         y_transformed = y.loc[X_transformed.index]
         return X_transformed, y_transformed
+
+
+class UnaryFeatureDropper(BaseEstimator, TransformerMixin):
+    """
+    A transformer that drops features with only one unique value.
+
+    Parameters:
+        None
+    """
+
+    def __init__(self):
+        self.features_to_drop = []
+
+    def fit(self, X, y=None):
+        """
+        Identify features with only one unique value.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to identify features.
+            y (pd.Series or None): (Ignored) Optional target variable.
+
+        Returns:
+            self: The fitted transformer.
+        """
+        self.features_to_drop = X.columns[X.nunique() == 1].tolist()
+        return self
+
+    def transform(self, X):
+        """
+        Drop features with only one unique value from the dataset.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to drop features.
+
+        Returns:
+            pd.DataFrame: The dataset with features dropped.
+        """
+        return X.drop(columns=self.features_to_drop)
+
+    def transform_x_y(self, X, y):
+        """
+        Drop features with only one unique value from the dataset and align the target accordingly.
+
+        Parameters:
+            X (pd.DataFrame): Input features.
+            y (pd.Series): Target variable.
+
+        Returns:
+            tuple: (X_transformed, y_transformed) where y_transformed is aligned with X_transformed.
+        """
+        X_transformed = self.transform(X)
+        y_transformed = y.loc[X_transformed.index]
+        return X_transformed, y_transformed
+
+
+class RelativeFeatureGenerator(BaseEstimator, TransformerMixin):
+    """
+    A transformer that generates relative features based on groupby columns.
+
+    Parameters:
+        variables (list): List of dictionaries with 'groupby' and 'target' keys.
+
+        The 'groupby' key should contain the column name to group by, and the 'target' key
+        should contain the column name to generate the relative feature for.
+    """
+
+    def __init__(self, variables: list):
+        self._validate_variables(variables)
+        self.variables = variables
+        self.groupby_col = None
+        self.target_col = None
+        self.aggregate_func = None
+        self.target_col = None
+
+    def _validate_variables(self, variables: list):
+        """
+        Check if the variables list contains dictionaries with 'groupby', 'target', 'agg' and 'name' keys.
+
+        Parameters:
+            variables (list): List of variables to validate.
+
+        Raises:
+            ValueError: If any variable is not a dictionary with 'groupby', 'target', 'agg' and 'name' keys.
+        """
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+        required_keys = {"groupby", "target", "agg", "name"}
+        for variable in variables:
+            if not all(key in variable for key in required_keys):
+                raise ValueError(
+                    "Each variable should be a dictionary with 'groupby', 'target', 'agg' and 'name' keys"
+                )
+
+    def fit(self, X, y=None):
+        """
+        Empty fit method to comply with the scikit-learn API.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate relative features.
+            y (pd.Series or None): (Ignored) Optional target variable.
+
+        Returns:
+            self: The fitted transformer
+        """
+        return self
+
+    def transform(self, X):
+        """
+        Generate relative features based on the specified groupby and target columns.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate relative features.
+
+        Returns:
+            pd.DataFrame: The dataset with relative features added.
+        """
+        for variable in self.variables:
+            self.groupby_col = variable["groupby"]
+            self.target_col = variable["target"]
+            self.aggregate_func = variable["agg"]
+            self.feature_name = variable["name"]
+            X = self._generate_relative_feature(X)
+        return X
+
+    def _generate_relative_feature(self, X):
+        """
+        Generate a relative feature based on the groupby and target columns.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate the relative feature.
+
+        Returns:
+            pd.DataFrame: The dataset with the relative feature added.
+        """
+        X = X.copy()
+        X[self.feature_name] = X.groupby(self.groupby_col)[self.target_col].transform(
+            lambda x: x / x.agg(self.aggregate_func)
+        )
+        return X
+
+
+class ProximityScoreGenerator(BaseEstimator, TransformerMixin):
+    def __init__(self, variables: list):
+        self._validate_variables(variables)
+        self.variables = variables
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+
+    def _validate_variables(self, variables: list):
+        """
+        Check if the variables list contains dictionaries with 'target', 'name' keys.
+
+        Parameters:
+            variables (list): List of variables to validate.
+
+        Raises:
+            ValueError: If any variable is not a dictionary with target', 'name' keys.
+        """
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+        required_keys = {"target", "name"}
+        for variable in variables:
+            if not all(key in variable for key in required_keys):
+                raise ValueError(
+                    "Each variable should be a dictionary with 'target', 'name' keys."
+                )
+
+    def fit(self, X, y=None):
+        """
+        Empty fit method to comply with the scikit-learn API.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate relative features.
+            y (pd.Series or None): (Ignored) Optional target variable.
+
+        Returns:
+            self: The fitted transformer
+        """
+        return self
+
+    def transform(self, X):
+        """
+        Generate relative features based on the specified groupby and target columns.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate relative features.
+
+        Returns:
+            pd.DataFrame: The dataset with relative features added.
+        """
+        for variable in self.variables:
+            self.target_col = variable["target"]
+            self.feature_name = variable["name"]
+            X = self._generate_proximity_score_feature(X)
+        return X
+
+    def _generate_proximity_score_feature(self, X):
+        """
+        Generate a relative feature based on the groupby and target columns.
+
+        Parameters:
+            X (pd.DataFrame): The dataset from which to generate the relative feature.
+
+        Returns:
+            pd.DataFrame: The dataset with the relative feature added.
+        """
+        X = X.copy()
+        proximity = 1 - self.scaler.fit_transform(
+            pd.DataFrame(X[self.target_col].min(axis=1))
+        )
+        X[self.feature_name] = proximity.mean(axis=1)
+        return X
